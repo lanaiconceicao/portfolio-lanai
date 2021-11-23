a9fd8eb745a2c5655d92381bbdef9b1a6f4ccc76
Lanai Conceição <conceicao.lanai@gmail.com>
2021-11-23 18:04:32 -0300

    Css created

--- src/App.js
+++ src/App.js
@@ -5,31 +5,39 @@ import Button from './components/Button';
 
 function App() {
   return (
-    <>
-      <div className="profile-picture"></div>
-      <h1>Lanai Conceição</h1>
-      <h2>Web Development Student</h2>
-      <div className="button-container">
+    <div className="App">
+      <header className="header-container">
+        <div className="profile-picture"></div>
+        <div className="titles-container">
+          <h1 className="title">Lanai Conceição</h1>
+          <h2 className="subtitle">Web Development Student</h2>
+        </div>
+      </header>
+
+      <main className="button-container">
         <Button
-          icon={<FaLinkedin/>}
+          icon={<FaLinkedin className="icon" />}
           link="https://www.linkedin.com/in/lanai-caroline/"
           className="link-button"
           id="LinkedIn"
         />
         <Button
-          icon={<FaGithub/>}
+          icon={<FaGithub className="icon" />}
           link="https://github.com/lanaiconceicao"
           className="link-button"
           id="GitHub"
         />
         <Button
-          icon={<FiMail/>}
+          icon={<FiMail className="icon" />}
           link="mailto:conceicaolanai@gmail.com"
           className="link-button"
           id="Email"
         />
-      </div>
-    </>
+      </main>
+      <footer>
+        <span className="span-footer">Developed by <a href="https://github.com/lanaiconceicao/portfolio-lanai">Lanai</a> ♥</span>
+      </footer>
+    </div>
   );
 }
 
