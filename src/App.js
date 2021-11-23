62470022c385fc3ed8db65bcc5bd12a9938fcab8
Lanai Conceição <conceicao.lanai@gmail.com>
2021-11-23 16:44:23 -0300

    LinkedIn button, Github button and Email button created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+import Button from './components/Button';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <>
+      <div className="button-container">
+        <Button
+          icon={<FaLinkedin/>}
+          link="https://www.linkedin.com/in/lanai-caroline/"
+          className="link-button"
+          id="LinkedIn"
+        />
+        <Button
+          icon={<FaGithub/>}
+          link="https://github.com/lanaiconceicao"
+          className="link-button"
+          id="GitHub"
+        />
+        <Button
+          icon={<FiMail/>}
+          link="mailto:conceicaolanai@gmail.com"
+          className="link-button"
+          id="Email"
+        />
+      </div>
+    </>
   );
 }
 
